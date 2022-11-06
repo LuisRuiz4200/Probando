7b0a1c9dbef7668e762291b8847555c41b717f44
LuisRuiz4200 <lr27072001@gmail.com>
2022-11-06 18:02:36 -0500

    Proyecto de LP (ECLIPSE)
    Carpeta del proyecto de LP

---
+++ PROYECTO LP GRUPO C/MinisterioPúblico/src/clases/Participante.java
@@ -0,0 +1,83 @@
+package clases;
+
+public class Participante {
+	private String codPedido;
+	private String codParticipante;
+	private String entidad;
+	private String ruc;
+	private String correo;
+	private int telefono;
+	private String estado;
+	
+	public Participante(String codPedido, String codParticipante, String entidad, String ruc, String correo,
+			int telefono, String estado) {
+		
+		this.codPedido = codPedido;
+		this.codParticipante = codParticipante;
+		this.entidad = entidad;
+		this.ruc = ruc;
+		this.correo = correo;
+		this.telefono = telefono;
+		this.estado = estado;
+	}
+
+	public String getCodPedido() {
+		return codPedido;
+	}
+
+	public void setCodPedido(String codPedido) {
+		this.codPedido = codPedido;
+	}
+
+	public String getCodParticipante() {
+		return codParticipante;
+	}
+
+	public void setCodParticipante(String codParticipante) {
+		this.codParticipante = codParticipante;
+	}
+
+	public String getEntidad() {
+		return entidad;
+	}
+
+	public void setEntidad(String entidad) {
+		this.entidad = entidad;
+	}
+
+	public String getRuc() {
+		return ruc;
+	}
+
+	public void setRuc(String ruc) {
+		this.ruc = ruc;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+	
+	
+}
---
+++ PROYECTO LP GRUPO C/MinisterioPúblico/src/clases/Propuesta.java
@@ -0,0 +1,101 @@
+package clases;
+
+public class Propuesta {
+	
+	private String codPedido;
+	private String codParticipante;
+	private String codPropuesta;
+	private String fecha;
+	private String propTecnica;
+	private String propEconomica;
+	private String estado;
+	
+	
+	public Propuesta(String codPedido, String codParticipante, String codPropuesta, String fecha, String propTecnica,
+			String propEconomica, String estado) {
+		
+		this.codPedido = codPedido;
+		this.codParticipante = codParticipante;
+		this.codPropuesta = codPropuesta;
+		this.fecha = fecha;
+		this.propTecnica = propTecnica;
+		this.propEconomica = propEconomica;
+		this.estado = estado;
+	}
+
+
+	public String getCodPedido() {
+		return codPedido;
+	}
+
+
+	public void setCodPedido(String codPedido) {
+		this.codPedido = codPedido;
+	}
+
+
+	public String getCodParticipante() {
+		return codParticipante;
+	}
+
+
+	public void setCodParticipante(String codParticipante) {
+		this.codParticipante = codParticipante;
+	}
+
+
+	public String getCodPropuesta() {
+		return codPropuesta;
+	}
+
+
+	public void setCodPropuesta(String codPropuesta) {
+		this.codPropuesta = codPropuesta;
+	}
+
+
+	public String getFecha() {
+		return fecha;
+	}
+
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+
+	public String getPropTecnica() {
+		return propTecnica;
+	}
+
+
+	public void setPropTecnica(String propTecnica) {
+		this.propTecnica = propTecnica;
+	}
+
+
+	public String getPropEconomica() {
+		return propEconomica;
+	}
+
+
+	public void setPropEconomica(String propEconomica) {
+		this.propEconomica = propEconomica;
+	}
+
+
+	public String getEstado() {
+		return estado;
+	}
+
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+	
+	
+	
+	
+}
