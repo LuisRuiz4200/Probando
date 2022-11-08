e6cf0de2742c0a84490683edbe426a64badf5088
JeanPierre23cpu <jean23.32naej@gmail.com>
2022-11-07 20:38:40 -0500

    asss
    ass

--- LPI_semana02_plantilla/LPI_semana02_plantilla/src/model/Producto.java
+++ LPI_semana02_plantilla/LPI_semana02_plantilla/src/model/Producto.java
@@ -9,6 +9,7 @@ public class Producto {
 //klisman gay 
 	//CONFIRMO
 	//A MI SI ME FUNCIONA
+	// Otra prueba
 	
 	public Producto(String cod, String prod, int cant, double pre, int tipo) {
 		this.cod = cod;
