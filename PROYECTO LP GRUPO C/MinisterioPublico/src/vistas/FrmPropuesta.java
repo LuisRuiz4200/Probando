e8b0977092665b851eed9ee8375a773baf293bf9
Ricardo <Ricardo@local>
2022-11-13 13:34:52 -0500

    Mejoras FrmConsultaPropuesta
    Mejoras FrmConsultaPropuesta

--- PROYECTO LP GRUPO C/MinisterioPublico/src/vistas/FrmPropuesta.java
+++ PROYECTO LP GRUPO C/MinisterioPublico/src/vistas/FrmPropuesta.java
@@ -50,9 +50,9 @@ public class FrmPropuesta extends JInternalFrame implements ActionListener, Item
 	private final ButtonGroup buttonGroupPT = new ButtonGroup();
 	private final ButtonGroup buttonGroupPE = new ButtonGroup();
 
-	PropuestaDAO gProp = new PropuestaDAO();
-	PedidoDAO gPed = new PedidoDAO();
-	ParticipanteDAO gPart = new ParticipanteDAO();
+	private PropuestaDAO gProp = new PropuestaDAO();
+	private PedidoDAO gPed = new PedidoDAO();
+	private ParticipanteDAO gPart = new ParticipanteDAO();
 
 	private JComboBox <Object>cboPedido;
 	private JComboBox <Object>cboParticipante;
@@ -208,7 +208,7 @@ public class FrmPropuesta extends JInternalFrame implements ActionListener, Item
 		txtPropuesta.setText("");
 		
 		if (list.size() == 0) {
-			txtPropuesta.setText("PD001");
+			txtPropuesta.setText("PR001");
 		} else {
 			String idProp = list.get(list.size() - 1).getCodPropuesta();
 
