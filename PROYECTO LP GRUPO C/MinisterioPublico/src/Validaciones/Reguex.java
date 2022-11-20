ec3b7e2cf94235bf292ba9006f8f71930e196fc4
JeanPierre23cpu <jean23.32naej@gmail.com>
2022-11-19 19:40:09 -0500

    Reguex

--- PROYECTO LP GRUPO C/MinisterioPublico/src/Validaciones/Reguex.java
+++ PROYECTO LP GRUPO C/MinisterioPublico/src/Validaciones/Reguex.java
@@ -4,22 +4,22 @@ public class Reguex {
 	
 	//PARTICIPANTE
 	
-	public static String ID_PARTICIPANTE="";
-	public static String ENTIDAD_PARTICIPANTE ="";
-	public static String RUC_PARTICIPANTE = "";
+	public static String ID_PARTICIPANTE="[P][D]\\d{3}";
+	public static String ENTIDAD_PARTICIPANTE ="[a-zA-Z\\s]{2,35}";
+	public static String RUC_PARTICIPANTE = "\\d{11}";
 	
-	public static String TELEFONO_PARTICIPANTE = "";
+	public static String TELEFONO_PARTICIPANTE = "\\d{9}";
 	public static String CORREO_PARTICIPANTE = "";
 	
 	
 	//MIEMBRO DEL CEP
 	
-	public static String ID_CEP = "";
-	public static String NOMBRE_CEP="";
-	public static String APELLIDO_CEP= "";
-	public static String DNI_CEP = "";
-	public static String FUNCION_CEP = "";
-	public static String DEPENDENCIA_CEP = "";
+	public static String ID_CEP = "[M][C]\\d{3}";
+	public static String NOMBRE_CEP="[a-zA-Z\\s]{2,20}";
+	public static String APELLIDO_CEP="[a-zA-Z\\ñ\\s]{2,20}";
+	public static String DNI_CEP = "\\d{8}";
+	public static String FUNCION_CEP ="[a-zA-Z\\s]{2,20}";
+	public static String DEPENDENCIA_CEP ="[a-zA-Z\\s]{2,20}";
 	
 	//PROPUESTA
 	
@@ -27,7 +27,10 @@ public class Reguex {
 	
 	//APELACION
 	
-	public static String ID_APELACION = "";
+	public static String ID_APELACION = "[A][P]\\d{3}";
+	public static String DESCRIPCION_APE=".{2,450}";
+	public static String ESTADO_APE="[a-zA-Z\\ñ\\s]{2,20}";
+	
 	
 	// EVALUACION DE PROPUESTA
 	public static String ID_EVALUACION="";
